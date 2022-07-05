06f5d87e39272deab303d53b98c9b8892f5fb2fe
LiuYinghao <liuyinghao4618@163.com>
2022-07-05 10:46:50 +0800

    feat(login): 临时updata

---
+++ src/views/login/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="contianer">
+    <el-form
+      ref="ruleFormRef"
+      :model="ruleForm"
+      :rules="rules"
+      label-width="120px"
+      class="demo-ruleForm"
+      :size="formSize"
+      status-icon
+    >
+      <el-form-item label="username" prop="username">
+        <el-input v-model="ruleForm.username" />
+      </el-form-item>
+      <el-form-item label="password" prop="password">
+        <el-input v-model="ruleForm.password" />
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script setup>
+import { reactive } from 'vue'
+const ruleForm = reactive({
+  username: '',
+  password: ''
+})
+const rules = reactive({
+  name: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 3, max: 5, message: '长度为3-5位', trigger: 'blur' }
+  ]
+})
+</script>
+<style lang="scss" scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    open:true,
+    port:4618,
+    https:false,
+    host:'localhost',
+    proxy:{
+      
+    }
+  }
 })
